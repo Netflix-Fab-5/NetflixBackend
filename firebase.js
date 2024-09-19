726830d43197bd3ad3b097be72f8a3e11f25a289
hannaholiero <hannahostebo@gmail.com>
2024-09-19 22:28:09 +0200

    Render up and running!
    Connected to Render
    Our backend server is live on: https://netflixbackend-z1k7.onrender.com/

--- firebase.js
+++ firebase.js
@@ -1,20 +1,17 @@
 // firebase.js
-const admin = require('firebase-admin');
-require('dotenv').config(); 
-
+const admin = require("firebase-admin");
+require("dotenv").config();
 
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Hantera radbrytningar
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Hantera radbrytningar
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
-
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
-
 
 const db = admin.database();
 module.exports = db;
