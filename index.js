f6c09182b338d8547ccb66f44c24c007684b3d2d
ulle73 <ulle73@users.noreply.github.com>
2024-09-19 22:43:43 +0000

    Prettified Code!

--- index.js
+++ index.js
@@ -6,19 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
