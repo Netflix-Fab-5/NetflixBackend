b9521fcc7b98ecc6607ff894568a7bb6e5808d9a
ulle73 <ulle73@users.noreply.github.com>
2024-09-20 14:54:54 +0000

    Prettified Code!

--- index.js
+++ index.js
@@ -6,18 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello World, we have now have Render up and running!");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
