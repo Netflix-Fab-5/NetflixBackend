60ff6c075f25ab80f09b7959ae1e83b0120555bd
ulle73 <rydqvist89@hotmail.com>
2024-09-21 01:04:21 +0200

    added cors for requests from frontend

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import movieRouter from "./routes/movieRouter.js";
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World, we have now have Render up and running!");
