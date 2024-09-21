344b150f009ae3ad324d7b5b8e360264908d79f1
Meena M <meenasingh754@gmail.com>
2024-09-21 14:07:41 +0200

    Added trendingMovies route
    resolved conflicts

--- middleware/movieMiddleware.js
+++ middleware/movieMiddleware.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Function for get all movies
 export const getAllMovies = (req, res) => {
   const filePath = path.join(__dirname, "..", "data", "movies.json");
 
@@ -18,3 +19,24 @@ export const getAllMovies = (req, res) => {
     res.json(movies);
   });
 };
+<<<<<<< HEAD
+=======
+
+// Function for get trending movies
+export const getAllTrendingMovies = (req, res) => { 
+  const filePath = path.join(__dirname, "..", "data", "movies.json");
+
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).send("Error reading the movie file");
+    }
+
+    const movies = JSON.parse(data);
+
+    // Filter out the trending movies
+    const trendingMovies = movies.filter(movie => movie.isTrending);
+
+    res.json(trendingMovies);
+  });
+}
+>>>>>>> 36dd008 (Added trendingMovies route)
--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { getAllMovies } from "../middleware/movieMiddleware.js";
+import { getAllMovies, getAllTrendingMovies } from "../middleware/movieMiddleware.js";
 
 const router = express.Router();
+
 // routes
 router.get("/", getAllMovies);
 
+<<<<<<< HEAD
 export default router;
+=======
+router.get("/trending", getAllTrendingMovies)
+
+export default router;
+>>>>>>> 36dd008 (Added trendingMovies route)
