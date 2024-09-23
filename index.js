49bcd7c312ea4d51c22ef054997b1719ca11f05e
hannaholiero <hannahostebo@gmail.com>
2024-09-23 11:24:18 +0200

    Added functionality for Render to work

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 const app = express();
 const port = process.env.PORT || 3000;
+const apiUrl = process.env.API_URL;
+
+app.use(
+  cors({
+    origin: apiUrl, // Använd URL:en från .env-filen
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World, we have now have Render up and running!");
 });
 
+// Starta servern
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  if (process.env.NODE_ENV === "production") {
+    console.log(`Server running in production mode on port ${port}`);
+  } else {
+    console.log(`Server running locally on http://localhost:${port}`);
+  }
 });
